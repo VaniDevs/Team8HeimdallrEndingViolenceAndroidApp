8c0e9d5bd282f763480922323710ae530dfa0c0e
Ricky Chen <rickyshen1991@hotmail.com>
2016-03-05 14:23:20 -0800

    finished tabview

--- mobile/src/main/java/ca/bcit/heimdallr/ending_violence/HomeActivity.java
+++ mobile/src/main/java/ca/bcit/heimdallr/ending_violence/HomeActivity.java
@@ -2,6 +2,12 @@ package ca.bcit.heimdallr.ending_violence;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +15,49 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Set a toolbar which will replace the action bar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Setup the viewPager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        // Setup the Tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        // By using this method the tabs will be populated according to viewPager's count and
+        // with the name from the pagerAdapter getPageTitle()
+      //  tabLayout.setTabsFromPagerAdapter(pagerAdapter);
+        // This method ensures that tab selection events update the ViewPager and page changes update the selected tab.
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0: return Fragment1.newInstance();
+                case 1: return Fragment2.newInstance();
+                default: return Fragment1.newInstance();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tab " + (position + 1);
+        }
     }
 }
