7a42de564a07c1f2123989791d93c2851b8a1885
Anthony Smith <anthony@smith.lt>
2016-03-05 11:37:50 -0800

    Initial Commit

---
+++ mobile/src/main/java/ca/bcit/heimdallr/ending_violence/HomeActivity.java
@@ -0,0 +1,13 @@
+package ca.bcit.heimdallr.ending_violence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
