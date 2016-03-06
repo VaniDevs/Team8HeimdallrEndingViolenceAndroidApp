641fd368f898f357c3fefbde7f4a12987af0d4fc
Jan Ycasas <ycasas.jan@gmail.com>
2016-03-05 17:05:28 -0800

    Added the edit Stuff for profile

--- mobile/src/main/java/ca/bcit/heimdallr/ending_violence/Fragment2.java
+++ mobile/src/main/java/ca/bcit/heimdallr/ending_violence/Fragment2.java
@@ -2,9 +2,19 @@ package ca.bcit.heimdallr.ending_violence;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
+
 
 /**
  * Created by Owner on 3/5/16.
@@ -17,6 +27,11 @@ public class Fragment2 extends Fragment {
      *
      * @return A new instance of fragment Fragment2.
      */
+
+    LinearLayout childLayoutV;
+    LinearLayout prevThreatsV;
+    LinearLayout currentLL;
+
     public static Fragment2 newInstance() {
         return new Fragment2();
     }
@@ -33,7 +48,69 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_fragment2,
+                container, false);
+
+        /* Add Child */
+        childLayoutV = (LinearLayout) view.findViewById(R.id.ChildrenLayoutV);
+
+        Button childButton = (Button) view.findViewById(R.id.addChildButton);
+        childButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                childLayoutV.addView(createNewLinearLayoutH());
+                currentLL.addView(createNewTextView("Name", false));
+                currentLL.addView(createNewTextView("Phone Number", false));
+            }
+        });
+
+        /* Add PrevThreats */
+        prevThreatsV = (LinearLayout) view.findViewById(R.id.PreviousThreatsV);
+        Button prevButton = (Button) view.findViewById(R.id.addPreviousThreats);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prevThreatsV.addView(createNewTextView("Name", true));
+                prevThreatsV.addView(createNewLinearLayoutH());
+                currentLL.addView(createNewTextView("Licence Plate",false));
+                currentLL.addView(createNewCheckBox());
+            }
+        });
+
+        return view;
+    }
+
+    private LinearLayout createNewLinearLayoutH(){
+        LinearLayout LL = new LinearLayout(getActivity());
+        final LayoutParams lparams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        LL.setLayoutParams(lparams);
+        LL.setOrientation(LinearLayout.HORIZONTAL);
+        LL.setId(View.generateViewId());
+        currentLL = LL;
+        return LL;
+    }
+
+    private EditText createNewTextView(String text, boolean full) {
+        int x;
+        if(!full){
+            x = ((getActivity().getResources().getDisplayMetrics().widthPixels)/2 - 50);
+        } else {
+            x = (getActivity().getResources().getDisplayMetrics().widthPixels);
+        }
+        final LayoutParams lparams = new LayoutParams(x, LayoutParams.MATCH_PARENT);
+        final EditText editText = new EditText(getActivity());
+        editText.setText(text);
+        editText.setLayoutParams(lparams);
+        return editText;
+    }
+
+    private CheckBox createNewCheckBox(){
+        CheckBox cb = new CheckBox(getActivity());
+        final LayoutParams lparams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        cb.setLayoutParams(lparams);
+        cb.setText("Access To Weapons?");
+        return cb;
     }
 }
---
+++ mobile/src/main/java/ca/bcit/heimdallr/ending_violence/Fragment3.java
@@ -0,0 +1,38 @@
+package ca.bcit.heimdallr.ending_violence;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Owner on 3/5/16.
+ */
+public class Fragment3 extends Fragment {
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment Fragment1.
+     */
+    public static Fragment1 newInstance() {
+        return new Fragment1();
+    }
+
+    public Fragment3() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment3, container, false);
+    }
+}
